05384b86e1127609c6fca2d00a0a71c0c597bcfa
Kcronnos <leog42119@gmail.com>
2024-08-21 22:45:21 -0300

    Revert "Ativação e Atualização"
    This reverts commit 61a1ebcaa1ae92ee86d9c61c577dee22e25cd15d.

--- src/view/JPrincipal.java
+++ src/view/JPrincipal.java
@@ -14,8 +14,6 @@ import java.awt.Font;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.awt.event.ActionEvent;
 
 public class JPrincipal extends JFrame {
@@ -44,16 +42,6 @@ public class JPrincipal extends JFrame {
 	 * Create the frame.
 	 */
 	public JPrincipal() {
-		JOptionPane.showMessageDialog(null, "No momento, vamos verificar se há atualizações. Aguarde um pouco!",
-				"Atualizador",
-				JOptionPane.INFORMATION_MESSAGE);
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-		}
-		JOptionPane.showMessageDialog(null, "Não há atualizações no momento!", "Atualizador",
-				JOptionPane.INFORMATION_MESSAGE);
-
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 630, 520);
 		contentPane = new JPanel();
@@ -61,19 +49,19 @@ public class JPrincipal extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-
+		
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(64, 128, 128));
 		panel.setBounds(73, 21, 480, 443);
 		contentPane.add(panel);
 		panel.setLayout(null);
-
+		
 		JLabel lblNewLabel = new JLabel("Bem-Vindo");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 20));
 		lblNewLabel.setBounds(164, 71, 135, 25);
 		panel.add(lblNewLabel);
-
+		
 		JButton btnNewButton = new JButton("ESTOQUE");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -86,7 +74,7 @@ public class JPrincipal extends JFrame {
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 15));
 		btnNewButton.setBounds(141, 168, 181, 39);
 		panel.add(btnNewButton);
-
+		
 		JButton btnVenda = new JButton("VENDAS");
 		btnVenda.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -99,7 +87,7 @@ public class JPrincipal extends JFrame {
 		btnVenda.setFont(new Font("Tahoma", Font.BOLD, 15));
 		btnVenda.setBounds(141, 219, 181, 39);
 		panel.add(btnVenda);
-
+		
 		JButton btnGerencia = new JButton("GERENCIA");
 		btnGerencia.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -112,57 +100,31 @@ public class JPrincipal extends JFrame {
 		btnGerencia.setFont(new Font("Tahoma", Font.BOLD, 15));
 		btnGerencia.setBounds(141, 268, 181, 39);
 		panel.add(btnGerencia);
-
+		
 		JLabel lblNewLabel_1 = new JLabel("Selecione O Setor");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_1.setBounds(141, 136, 181, 13);
 		panel.add(lblNewLabel_1);
-
+		
 		JLabel lblNewLabel_2 = new JLabel("StoSALE");
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 32));
 		lblNewLabel_2.setBounds(157, 41, 148, 31);
 		panel.add(lblNewLabel_2);
-
-		JLabel lblNewLabel_3 = new JLabel("stockreserveoficial@gmail.com");
+		
+		JLabel lblNewLabel_3 = new JLabel("Trade&Reseve@gmail.com");
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblNewLabel_3.setBounds(302, 406, 157, 13);
 		panel.add(lblNewLabel_3);
-
+		
 		JLabel lblNewLabel_4 = new JLabel("SAC");
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNewLabel_4.setBounds(302, 389, 45, 13);
 		panel.add(lblNewLabel_4);
-
+		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBounds(302, 384, 148, 35);
 		panel.add(panel_1);
-
-		JButton btnAtivarLicenca = new JButton("Ativar Licença");
-		btnAtivarLicenca.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String chaveSerie = JOptionPane.showInputDialog("Digite a chave de série:");
-				String usuarioRegistrado = JOptionPane.showInputDialog("Digite o usuário registrado:");
-
-				if (chaveSerie != null && usuarioRegistrado != null) {
-					try (FileWriter writer = new FileWriter("licenca.txt")) {
-						writer.write(chaveSerie + "\n" + usuarioRegistrado);
-						writer.flush();
-					} catch (IOException ex) {
-						JOptionPane.showMessageDialog(null, "Erro ao salvar informações de licença.");
-						return;
-					}
-					// Como é apenas uma questão de "idéia", o programa não carregará de fato uma licença toda vez que abrir. Mas tá aí. :P
-
-					JOptionPane.showMessageDialog(null, "Licença ativada com sucesso.");
-					btnAtivarLicenca.setText("Ativado!");
-					btnAtivarLicenca.setEnabled(false);
-				}
-			}
-		});
-		btnAtivarLicenca.setFont(new Font("Tahoma", Font.BOLD, 12));
-		btnAtivarLicenca.setBounds(302, 350, 148, 23);
-		panel.add(btnAtivarLicenca);
 	}
 }
